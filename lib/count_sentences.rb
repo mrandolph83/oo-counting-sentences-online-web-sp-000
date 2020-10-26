16b9fd6ae73549c57c7e5f41bb7841486000c123
Matt Smith <mrandolph83@gmail.com>
2020-10-26 15:30:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-   return new_array.length
+  array = self.split(/[\.!?] /)
+    array.length
   end
 end
